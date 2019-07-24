84c2bb2739f93ba3d10efad4f50185e5042b0815
santana <1220288430@qq.com>
2019-07-24 20:51:56 +0800

    fitst

--- relearn/ViewController.m
+++ relearn/ViewController.m
@@ -17,6 +17,10 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    UIView *view = [UIView new];
+    [view removeFromSuperview];
+    
+    
 }
 
 
